a4131fc4f8e766365b92152242c754bdf06bd561
Gautam Adatra <gautamkiran123@gmail.com>
2022-12-29 02:30:19 +0530

    chore: authContext update

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -4,7 +4,7 @@ import { createContext, useContext, useState } from "react";
 const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
-    const [ user, setUser ] = useState({users: null, token: null})
+    const [ user, setUser ] = useState({user: null, token: null})
     return(
         <AuthContext.Provider value={{user, setUser}}>
             {children}
