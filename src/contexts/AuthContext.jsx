84c619e50d18a63e884298895744a0fa275dba8d
Gautam Adatra <gautamkiran123@gmail.com>
2022-12-29 01:03:29 +0530

    chore: remove minor error from AuthContext

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { createContext, useContext, useState } from "react";
 
-const AuthContext = () => createContext();
+const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
     const [ user, setUser ] = useState({users: null, token: null})
     return(
-        <AuthContext.Provider>
+        <AuthContext.Provider value={{user, setUser}}>
             {children}
         </AuthContext.Provider>
     );
 }
 
-const useAuth = () => useContext();
+const useAuth = () => useContext(AuthContext);
 
-export {useAuth, AuthProvider}
+export {useAuth, AuthProvider};
