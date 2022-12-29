9d13f64f4e0964b9c98da5436462f2f1aacce762
Gautam Adatra <gautamkiran123@gmail.com>
2022-12-30 01:15:23 +0530

    add: video card for like page

---
+++ src/components/LikeCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const LikeCard = ({item}) => {
+    return(
+        <div>
+            <div className="video-container">
+                <img src={item.thumbnail} className="video-thumb" alt="video_thumbnail" />
+                <div className="vid-title">{item.title}</div>
+                <div className="creator-container">
+                <div className="vid-creator">{item.creator}</div>
+                <div className="vid-views">{item.views} views</div>
+                </div>
+            </div>
+        </div>
+    )
+}
