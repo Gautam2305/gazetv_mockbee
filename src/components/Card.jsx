516c32c9bc42b47f66b8d88658a9d671c7f8f424
Gautam Adatra <gautamkiran123@gmail.com>
2022-12-30 00:18:33 +0530

    chore: add buttons to card element

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,8 +5,19 @@ export const Card = ({item}) => {
             <div className="video-container">
                 <img src={item.thumbnail} className="video-thumb" alt="video_thumbnail" />
                 <div className="vid-title">{item.title}</div>
+                <div className="creator-container">
                 <div className="vid-creator">{item.creator}</div>
-                <div className="vid-views">{item.views}</div>
+                <div className="vid-views">{item.views} views</div>
+                </div>
+                <div className="extras-container">
+                    <div className="like-btn-container">
+                    <i class="fa-solid fa-thumbs-up like-btn"></i>
+                    <i class="fa-solid fa-thumbs-down like-btn"></i>
+                    </div>
+                
+                <i className="fa-solid fa-clock-rotate-left"></i>
+                </div>
+                
             </div>
         </div>
     )
