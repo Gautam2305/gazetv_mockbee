84c2bd8221c8163e18d2573704b33aecc3e93547
Gautam Adatra <gautamkiran123@gmail.com>
2022-12-30 19:39:19 +0530

    chore: watchlater context added

---
+++ src/contexts/WatchLaterContext.jsx
@@ -0,0 +1,16 @@
+import { createContext, useContext, useState } from "react";
+
+const WatchLaterContext = createContext();
+
+const WatchLaterProvider = ({children}) => {
+    const [watchlater, setWatchlater] = useState({watchlaterList: []})
+    return(
+        <WatchLaterContext.Provider value={{watchlater, setWatchlater}}>
+            {children}
+        </WatchLaterContext.Provider>
+    )
+}
+
+const useWatchLater = () => useContext(WatchLaterContext);
+
+export { WatchLaterProvider, useWatchLater};
